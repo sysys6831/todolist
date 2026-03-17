e2ad6c7ee6e7dbbb35953c069c5d62b9e1248fc9
youngsik <syssys6831@gmail.com>
2026-03-18 02:08:49 +0900

    20260318

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,20 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import "./App.css"; // 스타일을 적용하기 위해 불러옵니다.
+
+/*
+작성자 : 1ch
+작성일 : 2030.5.30.
+내용 : 기능에 대한 내용
+*/
 
 function App() {
+  let name = "리액트";
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+      <h1 className="test">
+        Hello,
+        {name === "리액트" ? <h1>YES</h1> : null}!!
+      </h1>
+      {/* <p>반갑습니다.</p> */}
     </div>
   );
 }
